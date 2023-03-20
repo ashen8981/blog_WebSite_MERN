0f2153e7157b72428c935d55aa02ddb83d98e286
ashen8981 <ashenmalindu112@gmail.com>
2023-03-20 09:04:50 +0530

    header/sidebar is done

---
+++ client/src/header/Header.jsx
@@ -0,0 +1,14 @@
+import "./header.css"
+import NodeImage from '../images/NodeImg1.jpg';
+
+export default function Header() {
+  return (
+    <div className='header'> 
+      <div className="headerTitles">
+        <span className="headerTitleSm">Node. js </span>
+        <span className="headerTitleLg">Blog</span>
+       </div>
+       <img className="headerImg" src ={NodeImage} alt="NodeImage"/>
+    </div>
+  )
+}
---
+++ client/src/sidebar/Sidebar.jsx
@@ -0,0 +1,37 @@
+import "./sidebar.css"
+import ME from "../images/ME.jpg"
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+        <div className="sidebarItem">
+          <span className="sidebarTitle">ABOUT ME</span>
+          <img src={ME} alt="" />
+          <p> A third-year IT diplomat at the Institute of Technology, University of Moratuwa (NDT).
+              I’m looking to collaborate on full-stack development.
+              I’m currently learning some frameworks and languages.
+          </p>
+        </div>
+        <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Sports</li>
+          <li className="sidebarListItem">Cinema</li>
+          <li className="sidebarListItem">Tech</li>
+        </ul>
+        </div>
+        <div className="sidebarItem">
+          <span className="sidebarTitle">FOLLOW US</span>
+          <div className="sidebarSocial">
+            <i className="sidebarIcon fa-brands fa-facebook"></i>
+            <i className="sidebarIcon fa-brands fa-square-twitter"></i>
+            <i className="sidebarIcon fa-brands fa-pinterest"></i> 
+            <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+        </div>
+        </div>
+    </div>
+  )
+}
