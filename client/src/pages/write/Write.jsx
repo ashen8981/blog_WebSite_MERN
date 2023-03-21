fdbd2630aa24f931f04bd4b9b3b8efa129592844
ashen8981 <ashenmalindu112@gmail.com>
2023-03-21 11:39:21 +0530

    write post page is done

---
+++ client/src/pages/write/Write.jsx
@@ -0,0 +1,31 @@
+import './write.css'
+import  WRITE from '../../images/write.jpg'
+
+export default function Write() {
+  return (
+    <div className='write'>
+        <img 
+        className='writeImg'
+        src={WRITE} 
+        alt="" 
+        />
+        <form className='writeForm'>
+            <div className="writeFormGroup">
+                <label htmlFor="fileInput">
+                <i className="writeIcon fa-sharp fa-solid fa-plus"></i>
+                </label>
+                <input type="file" id="fileInput" style={{display:'none'}}/>
+                <input type="text" placeholder='Title' className='writeInput' autoFocus={true}/>
+            </div>
+            <div className="writeFormGroup">
+                <textarea 
+                placeholder='Tell your story.....' 
+                type="text"
+                className="writeInput writeText"> 
+                </textarea>
+            </div>
+            <button className="writeSubmit">Publish</button>
+        </form>
+    </div>
+  )
+}
